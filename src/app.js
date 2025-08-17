6be4e0fc3a6db4be337a129d9c11847617f2cdae
Prince Shukla <princecodesandprojects@gmail.com>
2025-08-17 21:28:46 +0530

    added user retrieval endpoints and modified signup logic to accept user data from request body

--- src/app.js
+++ src/app.js
@@ -5,23 +5,45 @@ const User = require("./models/user.js");
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   try {
-    const user = new User({
-      firstName: "Prince",
-      lastName: "Shukla",
-      email: "prince@gmail.com",
-      phoneNumber: 1234567890,
-      password: "Test@123",
-      age: 24,
-      gender: "male",
-    });
+    const user = new User(req.body);
 
     const savedUser = await user.save();
 
     res
       .status(200)
       .json({ message: "User signed up successfully", data: savedUser });
+  } catch (error) {
+    console.error("Failed to create new user");
+    res.status(500).send("Failed to create user");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const email = req.body.email;
+
+    const users = await User.findOne({ email });
+
+    if (!users) {
+      res.status(404).send("No user found");
+    } else {
+      res.status(200).json({ message: "user get successfully", data: users });
+    }
+  } catch (error) {
+    console.error("Failed to create new user");
+    res.status(500).send("Failed to create user");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.status(200).json({ message: "user get successfully", data: users });
   } catch (error) {
     console.error("Failed to create new user");
     res.status(500).send("Failed to create user");
