0ec5aeaa94dc6e7803c0acdfb3c7820b325d981d
Prince Shukla <princecodesandprojects@gmail.com>
2025-08-17 16:21:06 +0530

    created use model

---
+++ src/models/user.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      maxLength: 50,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      maxLength: 100,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      maxLength: 20,
+      validate(value) {
+        if (!["male", "female"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      maxLength: 20,
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
